cc37adfd38a9f107061859cb5cc03cc3d57b6b71
Maryam Bisadi <39537957+MaryamBisadi@users.noreply.github.com>
2020-06-18 09:11:42 -0700

    Update featureExtraction.py

--- data-processing/featureExtraction.py
+++ data-processing/featureExtraction.py
@@ -1,3 +1,8 @@
+from pyspark import SQLContext
+from pyspark.sql import SparkSession
+from pyspark.sql import *
+from pyspark.sql.functions import kurtosis,col
+from pyspark.sql.types import *
 import os
 import boto3
 import wfdb, wfdb.processing
@@ -7,6 +12,7 @@ import numpy as np
 from scipy import stats
 import pandas as pd
 import csv
+import threading
 
 def readFromS3(bucket, prefix):
     patientList = []
@@ -32,8 +38,22 @@ def deleteTmpFile(record):
         print(record)
         print("can not remove the temp record")
 
-for i in range(0, len(patientRecords), 2):
+def TrainFeatureExtraction(sqlContext, patientRecords, f_schema, mortality, patientId):
+    f_min=-1
+    f_max=-1
+    f_mean=-1
+    f_median=-1
+    f_mode=-1
+    f_std=-1
+    f_kurtosis=-1
+    seg_count=0
+    heart_rate = []
+    i=0
 
+    data = [(0.0, 0.0,0.0, 0.0, 0.0, 0.0,0.0, 0, 0)]
+    df_features = sqlContext.createDataFrame(data, f_schema)
+
+    for i in range(0, len(patientRecords), 2):
         if len(patientRecords[i]['file_name']) == 32 and 'layout' not in patientRecords[i]['file_name']:
             writeTmpFile(patientRecords[i])
             if  'layout' not in patientRecords[i+1]['file_name']:
@@ -45,40 +65,50 @@ for i in range(0, len(patientRecords), 2):
             if  'layout' not in patientRecords[i+1]['file_name']:
                 deleteTmpFile(patientRecords[i+1]['file_name'][-16:])
 
-            if fields['sig_len'] < 200 or fields['sig_len'] > 100000:# > 10000 for test
+            if fields['sig_len'] < 200:
                 continue
 
             # preprocessing: nan to 0 for missing signals in the wave
             for channel in range(len(signals[0])):
-                signals[:,channel] = [0 if math.isnan(x) else x for x in signals[:,channel]] # preprocessing: nan -> 0
+                signals[:,channel] = [0.0 if math.isnan(x) else x for x in signals[:,channel]] # preprocessing: nan -> 0
 
             # get HR
             qrs_inds = wfdb.processing.gqrs_detect(sig=signals[:,0], fs=fields['fs'])
             heart_rate_wfdb=wfdb.processing.compute_hr(fields['sig_len'],qrs_inds,fields['fs'])
 
             # preprocessing: nan to 0 for missing signals in HR
-            heart_rate_wfdb = [0 if math.isnan(x) else x for x in heart_rate_wfdb] # preprocessing: nan -> 0
+            heart_rate_wfdb = [0.0 if math.isnan(x) else x for x in heart_rate_wfdb] # preprocessing: nan -> 0
 
             heart_rate+=heart_rate_wfdb
-
             if len(heart_rate_wfdb)>200:
                 seg_count+=1
-            #for test
-            if seg_count > 3:
-                break
 
     if len(heart_rate)>200:
-        f_min = round(np.amin(heart_rate)*100)/100
-        f_max = round(np.amax(heart_rate)*100)/100
-        f_mean = round(np.mean(heart_rate)*100)/100
-        f_median = round(np.median(heart_rate)*100)/100
-        f_mode = round(stats.mode(heart_rate)[0][0]*100)/100
-        f_std = round(np.std(heart_rate)*100)/100
-        f_kurtosis = round(stats.kurtosis(heart_rate, fisher=True)*100)/100
+        hr_schema = StructType([StructField("hr", FloatType(), True)])
+        data = {'hr':heart_rate}
+        df = pd.DataFrame(data, columns = ['hr'])
+        df_heart_rate= sqlContext.createDataFrame(df, hr_schema)
+        f_min = round(df_heart_rate.groupBy().min('hr').collect()[0]['min(hr)']*100)/100
+        f_max = round(df_heart_rate.groupBy().max('hr').collect()[0]['max(hr)']*100)/100
+        f_mean = round(df_heart_rate.groupBy().agg({'hr':'mean'}).collect()[0]['avg(hr)']*100)/100
+        f_std = round(df_heart_rate.groupBy().agg({'hr':'stddev'}).collect()[0]['stddev(hr)']*100)/100
+        f_median = round(df_heart_rate.approxQuantile('hr', [0.5], 0.002)[0]*100)/100
+        df_tmp =df_heart_rate.groupBy('hr').count()
+        f_mode = round(df_tmp.orderBy(df_tmp['count'].desc()).collect()[0][0]*100)/100
+        f_kurtosis = df_heart_rate.select(kurtosis(df_heart_rate['hr'])).collect()[0]['kurtosis(hr)']
+        if math.isnan(f_kurtosis) == True:
+            f_kurtosis = 0.0
+        else:
+            round(f_kurtosis*100)/100
+        f_data = {'min':f_min, 'max':f_max, 'mean':f_mean, 'median':f_median, 'mode':f_mode,\
+                'std':f_std, 'kurtosis':f_kurtosis, 'mortality':mortality,'patient_id': patientId}
+        df_tmp = pd.DataFrame(f_data, columns = ['min','max','mean','median','mode',\
+                            'std','kurtosis', 'mortality','patient_id'])
+        df_features = sqlContext.createDataFrame(df_tmp, f_schema)
+    return  df_features
 
-    return  [f_min, f_max, f_mean, f_median, f_mode, f_std, f_kurtosis]
 
-def TestFeatureExtraction(patientRecords,segmentLength):
+def TestFeatureExtraction(sqlContext, patientRecords, df_features, f_schema, mortality, patientId): #,segmentLength):
     f_min=-1
     f_max=-1
     f_mean=-1
@@ -89,72 +119,84 @@ def TestFeatureExtraction(patientRecords,segmentLength):
     seg_count=0
     heart_rate = []
     i=0
+
+    data = [(0.0, 0.0,0.0, 0.0, 0.0, 0.0,0.0, 0, 0)]
+    df_features = sqlContext.createDataFrame(data, f_schema)
     for i in range(0, len(patientRecords), 2):
-   
-            writeTmpFile(patientRecords[i])
-            if  'layout' not in patientRecords[i+1]['file_name']:
+            if '.hea' or '.dat' in patientRecords[i]['file_name']:
+                writeTmpFile(patientRecords[i])
+            if '.hea' or '.dat' in patientRecords[i+1]['file_name']:
                 writeTmpFile(patientRecords[i+1])
 
-            signals, fields = wfdb.rdsamp(patientRecords[i]['file_name'][-16:-4])#'p000030/3524877_0001')#, sampfrom=8000, sampto=10000)#, channel_names =['PLETH','RESP'])
-
+            signals, fields = wfdb.rdsamp(patientRecords[i]['file_name'][-16:-4])
             deleteTmpFile(patientRecords[i]['file_name'][-16:])
             if  'layout' not in patientRecords[i+1]['file_name']:
                 deleteTmpFile(patientRecords[i+1]['file_name'][-16:])
 
-            if fields['sig_len'] < 200 or fields['sig_len'] > 100000:# > 10000 for test
+            if fields['sig_len'] < 200:
                 continue
 
             # preprocessing: nan to 0 for missing signals in the wave
             for channel in range(len(signals[0])):
-                signals[:,channel] = [0 if math.isnan(x) else x for x in signals[:,channel]] # preprocessing: nan -> 0
+                signals[:,channel] = [0.0 if math.isnan(x) else x for x in signals[:,channel]] # preprocessing: nan -> 0
 
             # get HR
             qrs_inds = wfdb.processing.gqrs_detect(sig=signals[:,0], fs=fields['fs'])
             heart_rate_wfdb=wfdb.processing.compute_hr(fields['sig_len'],qrs_inds,fields['fs'])
 
             # preprocessing: nan to 0 for missing signals in HR
-            heart_rate_wfdb = [0 if math.isnan(x) else x for x in heart_rate_wfdb] # preprocessing: nan -> 0
+            heart_rate_wfdb = [0.0 if math.isnan(x) else x for x in heart_rate_wfdb] # preprocessing: nan -> 0
+
 
             heart_rate+=heart_rate_wfdb
 
             if len(heart_rate_wfdb)>200:
                 seg_count+=1
 
-            #for test
-            if seg_count > 3:
+            period = 50000 #15000
+
+            if len(heart_rate) > period * 15:
                 break
 
-    if len(heart_rate)>200 and len(heart_rate)>7500*segmentLength:
-        HR_seg = heart_rate[:7500*segmentLength]
-        f_min = round(np.amin(HR_seg)*100)/100
-        f_max = round(np.amax(HR_seg)*100)/100
-        f_mean = round(np.mean(HR_seg)*100)/100
-        f_median = round(np.median(HR_seg)*100)/100
-        f_mode = round(stats.mode(HR_seg)[0][0]*100)/100
-        f_std = round(np.std(HR_seg)*100)/100
-        f_kurtosis = round(stats.kurtosis(HR_seg, fisher=True)*100)/100
+    features_list = []
+    #features_day = []
+    if len(heart_rate)>200: 
+        hr_schema = StructType([StructField("hr", FloatType(), True)])
+        for h in range(1,13):
+            if len(heart_rate) > period*h:
+                HR_seg = heart_rate[:period*h]
+                data = {'hr':HR_seg}
+                df = pd.DataFrame(data, columns = ['hr'])
+                df_heart_rate= sqlContext.createDataFrame(df, hr_schema)
+                f_min = round(df_heart_rate.groupBy().min('hr').collect()[0]['min(hr)']*100)/100
+                f_max = round(df_heart_rate.groupBy().max('hr').collect()[0]['max(hr)']*100)/100
+                f_mean = round(df_heart_rate.groupBy().agg({'hr':'mean'}).collect()[0]['avg(hr)']*100)/100
+                f_std = round(df_heart_rate.groupBy().agg({'hr':'stddev'}).collect()[0]['stddev(hr)']*100)/100
+                f_median = round(df_heart_rate.approxQuantile('hr', [0.5], 0.002)[0]*100)/100
+                df_tmp =df_heart_rate.groupBy('hr').count()
+                f_mode = round(df_tmp.orderBy(df_tmp['count'].desc()).collect()[0][0]*100)/100
+                f_kurtosis = df_heart_rate.select(kurtosis(df_heart_rate['hr'])).collect()[0]['kurtosis(hr)']
+                if math.isnan(f_kurtosis) == True:
+                    f_kurtosis = 0.0
+                else:
+                    round(f_kurtosis*100)/100
+            else:
+                f_min, f_max, f_mean, f_median, f_mode, f_std, f_kurtosis = -1, -1, -1, -1, -1, -1, -1
+
+            f_data = {'min':f_min, 'max':f_max, 'mean':f_mean, 'median':f_median, 'mode':f_mode,\
+                    'std':f_std, 'kurtosis':f_kurtosis, 'mortality':mortality,'patient_id': patientId}
+            df_tmp = pd.DataFrame(f_data, columns = ['min','max','mean','median','mode',\
+                    'std','kurtosis', 'mortality','patient_id'], index=f_data)
+            df_new_features = sqlContext.createDataFrame(df_tmp, f_schema)
+            df_features = df_features.union(df_new_features)
 
 
-    return  [f_min, f_max, f_mean, f_median, f_mode, f_std, f_kurtosis]
-    
-    def saveToCSV(featureList, fileName):
-    file = open(fileName, 'w+', newline='')
-    with file:
-        write = csv.writer(file)
-        write.writerows(featureList)
-
-def readFromCSV(fileName):
-    featureList = []
-    with open(fileName, newline='') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            featureList.append(row)
-    return featureList
-
-def main():
+    return  df_features
+                                                                                                                                                                                                  219,63        51%
+def main(spark, i):
 
     bucket = "mimic3waveforms3"
-    p = '1.0/p00/'
+    p = '1.0/p0'+ str(i) +'/'
 
     s3 = boto3.resource('s3')
     my_bucket = s3.Bucket("mimic3waveforms3")
@@ -167,68 +209,64 @@ def main():
     client = boto3.client('s3')
     patientInfoObj = client.get_object(Bucket=bucket, Key='PATIENTS.csv')
 
-    pn=0
+    f_schema = StructType([\
+    StructField('min', DoubleType(), True),\
+    StructField('max', DoubleType(), True),\
+    StructField('mean', DoubleType(), True),\
+    StructField('median', DoubleType(), True),\
+    StructField('mode', DoubleType(), True),\
+    StructField('std', DoubleType(), True),\
+    StructField('kurtosis', DoubleType(), True),\
+    StructField('mortality_flag', IntegerType(), True),\
+    StructField('patient_id', IntegerType(), True)])
 
+    sqlContext = SQLContext(spark)
+
+    data = [(0.0, 0.0,0.0, 0.0, 0.0, 0.0,0.0, 0, 0)]
+    df_features = sqlContext.createDataFrame(data, f_schema)
     df = pd.read_csv(patientInfoObj['Body'])
+
+    # for first 6 batch of records  extract train features
+
     for patient in patientList:
-        for object_summary in my_bucket.objects.filter(Prefix=patient):
-            patientRecord = {}
-            patientRecord['file_name']=object_summary.key
-            patientRecord['body']=object_summary.get()['Body'].read()
-            patientRecords.append(patientRecord)
-            print("Patient Record:",patientRecord['file_name'])
-        patientId =int(patient[-7:-1])
-        mortality = (df[df['SUBJECT_ID']==patientId]['EXPIRE_FLAG'])
-        extractedFeatures = TrainFeatureExtraction(patientRecords)
-        extractedFeatures.append(int(mortality.values))
-        extractedFeatures.append(patientId)
-        if extractedFeatures[0]!=-1:
-            features_train.append(extractedFeatures)
 
-        pn+=1
-        if pn>2:
-            pn=0
-            break
+       patientRecords = []
+       for object_summary in my_bucket.objects.filter(Prefix=patient):
+          patientRecord = {}
+          patientRecord['file_name']=object_summary.key
+          if len(patientRecord['file_name']) != 32 or 'layout' in patientRecord['file_name']:
+              continue                                                                                                              287,63        72%
+          patientRecord['body']=object_summary.get()['Body'].read()
+          patientRecords.append(patientRecord)
+       patientId =int(patient[-7:-1])
+       mortality = (df[df['SUBJECT_ID']==patientId]['EXPIRE_FLAG'])
 
-    saveToCSV(features_train, 'TrainFeatures/TrainingFeatures.csv')
+      # for first 6 batch of records  extract train features
+      if i < 6:   
+          df_new_features = TrainFeatureExtraction(sqlContext, patientRecords, f_schema, mortality, patientId)
+          df_features = df_features.union(df_new_features)
 
-    print("**********Extract feature for prediction")
-    # Extract feature for prediction
-    for patient in patientList:
-        features_prediction = []
-        for object_summary in my_bucket.objects.filter(Prefix=patient):
-            patientRecord = {}
-            patientRecord['file_name']=object_summary.key
-            if len(patientRecord['file_name']) != 32 or 'layout' in patientRecord['file_name']:
-                continue
-            patientRecord['body']=object_summary.get()['Body'].read()
-            patientRecords.append(patientRecord)
-            patientId = int(patient[-7:-1])
-            mortality = (df[df['SUBJECT_ID']==patientId]['EXPIRE_FLAG'])
-            for segmentCount in range(1,10):
-                extractedFeatures = TestFeatureExtraction(patientRecords,segmentCount)
-                extractedFeatures.append(int(mortality.values))
-                print("...",extractedFeatures)
-                if extractedFeatures[0]!=-1:
-                    features_prediction.append(extractedFeatures)
-                if extractedFeatures[0]==-1:
-                    break
-                print("Patient Record:",patientRecord['file_name'])
-                if extractedFeatures[0]!=-1:
-                    features_prediction.append(extractedFeatures)
-        if len(features_prediction)>0:
-            saveToCSV(features_prediction, 'TestFeaturet/TestFeatures_'+patient[-7:-1]+'.csv')
-        pn+=1
-        if pn>3:
-            break
+      # for last 2 batchs of records extract test features
+      else: 
+          df_new_features = TestFeatureExtraction(sqlContext, patientRecords, df_features, f_schema, int(mortality.values), patientId)
+          df_features = df_features.union(df_new_features)
+          df_features = df_features.filter(col('patient_id')>0)
+          df_features.write.csv("s3a://"+bucket+"/PredictFeatures/"+str(patientId))
 
-
-    print("***********************************")
+      if i < 6: 
+        df_features = df_features.filter(col('patient_id')>0)
+        df_features.write.csv("s3a://"+bucket+"/TrainFeatures")
 
 if __name__ == "__main__":
 
-    print("\n************Feature Extraction...\n")
-    main()
-    print("\n*************Finish.\n")
+    spark = SparkSession.builder \
+                        .master('spark://ec2-52-8-238-139.us-west-1.compute.amazonaws.com:7077') \
+                        .appName('Feature Extraction') \
+                        .config('spark.executors.memory', '4gb')\
+                        .config('spark.executor.cores, 3')\
+                        .getOrCreate()
 
-                                                                                                    1,9           Top
+    for i in range(8):
+        t = threading.Thread(target=main, args=(spark, i))
+        t.start()
+        print("\n************Feature Extraction thread"+ str(i) +"...\n")
